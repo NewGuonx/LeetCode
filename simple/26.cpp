b791b02ecdf6500bc5c00b153ab46cd26f36dd43
sonaspy <sonaspy@zju.edu.cn>
2019-03-25 18:27:17 +0800

    26

---
+++ simple/26.cpp
@@ -0,0 +1,30 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<stack>
+#include<queue>
+#include<numeric>
+
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+class Solution
+{
+  public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
