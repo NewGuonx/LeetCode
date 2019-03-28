b086d05f616517c04728a7a9bd84094979c85853
sonaspy <sonaspy@zju.edu.cn>
2019-03-28 15:59:13 +0800

    update

---
+++ simple/00.cpp
@@ -0,0 +1,89 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <numeric>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+void Insert(TreeNode *&root, int data)
+{
+    if (!root)
+    {
+        root = new TreeNode(data);
+        return;
+    }
+    if (data < root->val)
+        Insert(root->left, data);
+    else
+        Insert(root->right, data);
+}
+class Solution
+{
+  public:
+    vector<int> preOrder(TreeNode *root)
+    {
+        vector<int> result;
+        stack<const TreeNode *> s;
+        const TreeNode *p = root;
+        while (s.size() || p)
+        {
+            if (p)
+            {
+                result.push_back(p->val);
+                if (p->right)
+                    s.push(p->right);
+                p = p->left;
+            }
+            else
+            {
+                p = s.top();
+                s.pop();
+                result.push_back(p->val);
+                if (p->right)
+                    s.push(p->right);
+                p = p->left;
+            }
+        }
+        return result;
+    }
+};
+
+void order(TreeNode *root)
+{
+    if (!root)
+        return;
+    order(root->left);
+    order(root->right);
+    cout << root->val << " ";
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    TreeNode *root = nullptr;
+    int a[] = {10,8,11,6,15,2,5,9,7,3,1,13,14,4,12};
+    for (int i =0; i < 14; i++)
+        Insert(root, a[i]);
+    Solution s;
+    cout <<"Pre\n";
+    // order(root);
+    vector<int> v = s.preOrder(root);
+    for(auto i : v)cout << i << " ";
+    return 0;
+}
