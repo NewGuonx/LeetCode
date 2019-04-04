c8ae61d872b0ef1c39018d61dd2d12849367de4c
sonaspy <sonaspy@zju.edu.cn>
2019-04-05 00:50:17 +0800

    update

---
+++ simple/nimgame.cpp
@@ -0,0 +1,22 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<bits/stdc++.h>
+
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+class Solution
+{
+  public:
+    bool canWinNim(int n)
+    {
+        return n % 4 == 0;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
