f1088d6e6bcb28b87457d33e19dfcb95383ead84
sonaspy <sonaspy@zju.edu.cn>
2019-04-17 17:39:41 +0800

    update

---
+++ normal/containWithMostWater.cpp
@@ -0,0 +1,30 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<bits/stdc++.h>
+
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int res = -1;
+        for(int left = 0, right = height.size() - 1; left <= right;){
+            int s = (right - left) * min(height[left], height[right]);
+            res = max(res, s);
+            if(height[left] <= height[right])
+                left++;
+            else right--;
+        }
+        return res;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
