1944f99930325bc1d62a993009add3e18c1dd4a4
sonaspy <sonaspy@zju.edu.cn>
2019-04-28 18:02:28 +0800

    update

---
+++ normal/CombinationSum1.cpp
@@ -0,0 +1,44 @@
+
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
+
+class Solution
+{
+public:
+    vector<vector<int>> ans;
+    vector<int> tmp;
+
+public:
+    void solve(vector<int> &candidates, int remain_, int l, int r)
+    {
+        if (remain_ < 0 || l > r)
+            return;
+        if (remain_ == 0)
+            ans.push_back(tmp);
+        else
+            for (int i = l; i <= r; i++)
+            {
+                tmp.push_back(candidates[i]);
+                solve(candidates, remain_ - candidates[i], i, r);
+                tmp.pop_back();
+            }
+    }
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        solve(candidates, target, 0, candidates.size() - 1);
+        return ans;
+    }
+};
---
+++ normal/CombinationSum2.cpp
@@ -0,0 +1,50 @@
+
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
+
+class Solution
+{
+private:
+    vector<vector<int>> ans;
+    vector<int> tmp;
+
+public:
+    vector<vector<int>> combinationSum2(vector<int> &a, int target)
+    {
+        sort(a.begin(), a.end());
+        int start = 0, end = upper_bound(a.begin(), a.end(), target) - a.begin();
+        solve(a, start, end == a.size() ? end - 1 : end, target);
+        return ans;
+    }
+
+private:
+    void solve(vector<int> &a, int l, int r, int target)
+    {
+        if (target < 0)
+            return;
+        if (target == 0)
+            ans.push_back(tmp);
+        else
+            for (int i = l; i <= r && a[i] <= target; i++)
+            {
+                if (i != l && a[i] == a[i - 1])
+                    continue;
+                tmp.push_back(a[i]);
+                solve(a, i + 1, r, target - a[i]);
+                tmp.pop_back();
+            }
+    }
+};
