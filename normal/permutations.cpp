c78498f234c635133c8e63141a6aaa06d7eaffb8
sonaspy <sonaspy@zju.edu.cn>
2019-05-14 19:20:19 +0800

    update

---
+++ normal/permutations.cpp
@@ -0,0 +1,28 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        do
+        {
+            res.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        return res;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
---
+++ normal/rotateImage.cpp
@@ -0,0 +1,28 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        const int n = matrix.size();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+        for (int i = 0; i < n / 2; i++)
+            for (int j = 0; j < n; j++)
+                swap(matrix[i][j], matrix[n - 1 - i][j]);
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
