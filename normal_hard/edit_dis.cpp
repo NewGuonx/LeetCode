fa20d095271a1ad50c76e074157d27b314d7d764
sonaspy <sonaspy@zju.edu.cn>
2019-05-23 19:52:33 +0800

    update

---
+++ normal_hard/edit_dis.cpp
@@ -0,0 +1,37 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+class Solution
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        const int m = word1.size();
+        const int n = word2.size();
+        int f[n + 1][m + 1], i, j;
+        for (i = 0; i < n + 1; i++)
+            f[i][0] = i;
+        for (i = 0; i < m + 1; i++)
+            f[0][i] = i;
+        for (i = 1; i < n + 1; i++)
+        {
+            for (j = 1; j < m + 1; j++)
+            {
+                int mn = min(f[i - 1][j], f[i][j - 1]);
+                f[i][j] = word1[i - 1] == word2[j - 1] ? f[i - 1][j - 1] : min(mn, f[i - 1][j - 1]) + 1;
+            }
+        }
+        return f[n][m];
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
---
+++ normal_hard/setZeros.cpp
@@ -0,0 +1,35 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        const int m = matrix.size(), n = matrix[0].size();
+        vector<bool> row(m, false), col(n, false);
+        int i, j;
+        for (i = 0; i < m; i++)
+            for (j = 0; j < n; j++)
+                if (!matrix[i][j])
+                    row[i] = col[j] = true;
+        for (i = 0; i < m; i++)
+            if (row[i])
+                fill(&matrix[i][0], &matrix[i][0] + n, 0);
+        for (j = 0; j < n; j++)
+            if (col[j])
+                for (i = 0; i < m; i++)
+                    matrix[i][j] = 0;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    return 0;
+}
