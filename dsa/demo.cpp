6d2a3fd008f6ca8933df2d787fa7fba87003af43
sonaspy <sonaspy@zju.edu.cn>
2019-09-15 11:16:17 +0800

    update

---
+++ dsa/demo.cpp
@@ -0,0 +1,17 @@
+// author -sonaspy@outlook.com
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+
+    
+    return 0;
+}
